5ae0a63b4d303d8b124b5e9ad65371ca6503dc1a
Ivan <ivan1501@gmail.com>
2020-02-04 10:54:53 +0100

    Update in function with headed and footer

--- functions.php
+++ functions.php
@@ -11,3 +11,33 @@ function pdo_connect_mysql() {
     	die ('Failed to connect to database!');
     }
 }
+
+// Template header
+function template_header($title) {
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>$title</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body>
+    <nav class="navtop">
+    	<div>
+    		<h1>Image Gallery</h1>
+            <a href="index.php"><i class="fas fa-image"></i>Gallery</a>
+    	</div>
+    </nav>
+EOT;
+}
+
+// Template footer
+function template_footer() {
+echo <<<EOT
+    </body>
+</html>
+EOT;
+}
+?>
